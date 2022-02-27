e1fda5bf8e0d74af8060c563d020e712bb044418
BPsoda <bpsoda@163.com>
2022-02-27 16:29:37 +0800

    Update polynomial_matching.py

--- polynomial_matching.py
+++ polynomial_matching.py
@@ -1,25 +1,41 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 class polyCoeff:
+    def __init__(self) -> None:
+        self.M = np.zeros([11, 3])
+
     def __init__(self, src, tgt) -> None:
         '''The class of polynomial matching coefficient.'''
-        self.src = src
-        self.target = tgt
+        self.src = src.astype(np.float64)
+        self.target = tgt.astype(np.float64)
         self.M = self.match()
 
-    def to_npy(self):
+    def to_npy(self, file_path):
         '''Save the matrix to .npy file'''
+        with open(file_path, 'wb') as f:
+            np.save(f, self.M)
 
-    def read_npy(self):
+    def read_npy(self, file_path):
         '''Read .npy file'''
+        with open(file_path, 'rb') as f:
+            self.M = np.load(f)
 
     def match(self):
         '''Given source colors and destination colors, calculate and return the coefficient matrix'''
-        h = np.array()
+        #  [r g b rg rb gb r2 g2 b2 rgb 1].
+        h = np.array([self.src[:,0], self.src[:,1], self.src[:,2], self.src[:,0]*self.src[:,1], self.src[:,0]*self.src[:,2], self.src[:,1]*self.src[:,2],
+        self.src[:,0]*self.src[:,0], self.src[:,1]*self.src[:,1], self.src[:,2]*self.src[:,2], self.src[:,0]*self.src[:,1]*self.src[:,3], np.ones(self.src[:,0].shape())])
+        regr = LinearRegression(fit_intercept=False).fit(h, self.target)
+        return regr.coef_
+
 
     def transform(self, img):
         '''Given source image and polynomial matching coefficient, return the transformed image.'''
-        pass
+        h_img = np.array([img[:,0], img[:,1], img[:,2], img[:,0]*img[:,1], img[:,0]*img[:,2], img[:,1]*img[:,2],
+        img[:,0]*img[:,0], img[:,1]*img[:,1], img[:,2]*img[:,2], img[:,0]*img[:,1]*img[:,3], np.ones(img[:,0].shape())])
+        return np.matmul(h_img, self.M)
+       
 
 ColorChecker2005 = np.array([[107,82,70],
 [184,146,129],
@@ -44,4 +60,4 @@ ColorChecker2005 = np.array([[107,82,70],
 [160,161,162],
 [120,120,121],
 [84,85,86],
-[52,53,54]])
+[52,53,54]], dtype=np.float64)
