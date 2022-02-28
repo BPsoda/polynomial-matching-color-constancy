4d29edd11d9002fa1a2e76aae455482460ea830b
BPsoda <bpsoda@163.com>
2022-02-28 12:03:23 +0800

    Complete standard conversion

--- polynomial_matching.py
+++ polynomial_matching.py
@@ -7,9 +7,7 @@ class polyCoeff:
 
     def __init__(self, src, tgt) -> None:
         '''The class of polynomial matching coefficient.'''
-        self.src = src.astype(np.float64)
-        self.target = tgt.astype(np.float64)
-        self.M = self.match()
+        self.M = self.match(src, tgt)
 
     def to_npy(self, file_path):
         '''Save the matrix to .npy file'''
@@ -21,43 +19,76 @@ class polyCoeff:
         with open(file_path, 'rb') as f:
             self.M = np.load(f)
 
-    def match(self):
+    def match(self, src, tgt):
         '''Given source colors and destination colors, calculate and return the coefficient matrix'''
         #  [r g b rg rb gb r2 g2 b2 rgb 1].
-        h = np.array([self.src[:,0], self.src[:,1], self.src[:,2], self.src[:,0]*self.src[:,1], self.src[:,0]*self.src[:,2], self.src[:,1]*self.src[:,2],
-        self.src[:,0]*self.src[:,0], self.src[:,1]*self.src[:,1], self.src[:,2]*self.src[:,2], self.src[:,0]*self.src[:,1]*self.src[:,3], np.ones(np.shape(self.src[:,0]))])
-        regr = LinearRegression(fit_intercept=False).fit(h, self.target)
+        h = np.array([src[:,0], src[:,1], src[:,2], src[:,0]*src[:,1], src[:,0]*src[:,2], src[:,1]*src[:,2],
+        src[:,0]*src[:,0], src[:,1]*src[:,1], src[:,2]*src[:,2], src[:,0]*src[:,1]*src[:,2], np.ones(np.shape(src[:,0]))]).T
+        regr = LinearRegression(fit_intercept=False).fit(h, tgt)
         return regr.coef_
 
 
     def transform(self, img):
         '''Given source image and polynomial matching coefficient, return the transformed image.'''
-        h_img = np.array([img[:,0], img[:,1], img[:,2], img[:,0]*img[:,1], img[:,0]*img[:,2], img[:,1]*img[:,2],
-        img[:,0]*img[:,0], img[:,1]*img[:,1], img[:,2]*img[:,2], img[:,0]*img[:,1]*img[:,3], np.ones(np.shape(img[:,0]))])
-        return np.matmul(h_img, self.M)
+        h_img = np.array([img[:,:,0], img[:,:,1], img[:,:,2], img[:,:,0]*img[:,:,1], img[:,:,0]*img[:,:,2], img[:,:,1]*img[:,:,2],
+        img[:,:,0]*img[:,:,0], img[:,:,1]*img[:,:,1], img[:,:,2]*img[:,:,2], img[:,:,0]*img[:,:,1]*img[:,:,2], np.ones(np.shape(img[:,:,0]))]).T
+        rtn = np.matmul(h_img, self.M.T)
+        rtn[rtn>255] = 255
+        rtn[rtn<0] = 0
+        return rtn.astype(np.uint8).swapaxes(0,1)
        
-
-ColorChecker2005 = np.array([[107,82,70],
-[184,146,129],
-[101,122,153],
-[95,107,69],
-[128,127,173],
-[129,188,171],
-[201,123,56],
-[77,92,166],
-[174,83,97],
-[86,61,104],
-[167,188,75],
-[213,160,55],
+# 18 19 20 21 22 23
+# 12 13 14 15 16 17
+# 6  7  8  9  10 11
+# 0  1  2  3  4  5
+ColorChecker2005_Adobe = np.array([[245,245,242],
+[200,201,201],
+[160,161,162],
+[120,120,121],
+[84,85,86],
+[52,53,54],
 [49,65,143],
 [99,148,80],
 [155,52,59],
 [227,197,52],
 [169,85,147],
 [61,135,167],
-[245,245,242],
-[200,201,201],
-[160,161,162],
-[120,120,121],
-[84,85,86],
-[52,53,54]], dtype=np.float64)
+[201,123,56],
+[77,92,166],
+[174,83,97],
+[86,61,104],
+[167,188,75],
+[213,160,55],
+[107,82,70],
+[184,146,129],
+[101,122,153],
+[95,107,69],
+[128,127,173],
+[129,188,171]], dtype=np.float64)
+
+ColorChecker2005_sRGB = np.array([
+    [245,245,243],
+    [200,202,202],
+    [161,163,163],
+    [121,121,122],
+    [82,84,86],
+    [49,49,51],
+    [35,63,147],
+    [67,149,74],
+    [180,49,57],
+    [238,198,20],
+    [193,84,151],
+    [0,136,170],
+    [224,124,47],
+    [68,91,170],
+    [198,82,97],
+    [94,58,106],
+    [159,189,63],
+    [230,162,39],
+    [116,81,67],
+    [199,147,129],
+    [91,122,156],
+    [90,108,64],
+    [130,128,176],
+    [92,190,172]
+])
